e5bacc1b9fc592def06b73dc0870b1721691246b
sorapmas <4mberamparo16@gmail.com>
2023-08-14 18:33:24 -0400

    updated query

--- csv_queries.sql
+++ csv_queries.sql
@@ -5,20 +5,9 @@ CREATE TABLE Departments (
     dept_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE EmployeeDepartment (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR(5) NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
-	PRIMARY KEY(emp_no, dept_no)
-);
-
-CREATE TABLE Managers (
-    dept_no VARCHAR(5) NOT NULL,
-    emp_no INT NOT NULL,
-    FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
-    FOREIGN KEY (emp_no) REFERENCES EmployeeDepartment(emp_no),
-	PRIMARY KEY(dept_no, emp_no)
+CREATE TABLE Title (
+    title_id VARCHAR(5) PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Employees (
@@ -38,10 +27,25 @@ CREATE TABLE Salary (
     FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
-CREATE TABLE Title (
-    title_id VARCHAR(5) PRIMARY KEY,
-    title VARCHAR(30) NOT NULL
+CREATE TABLE EmployeeDepartment (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(5),
+	PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES EmployeeDepartment(dept_no)
 );
+
+CREATE TABLE Managers (
+    dept_no VARCHAR(5) NOT NULL,
+    emp_no INT NOT NULL,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (emp_no, dept_no) REFERENCES EmployeeDepartment(emp_no, dept_no)
+);
+
+
+
+
+
 
 
 
