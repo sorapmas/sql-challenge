dfea7802b2c172b0a46e6800fa98cba650279c96
sorapmas <4mberamparo16@gmail.com>
2023-08-14 22:07:23 -0400

    updates

--- csv_queries.sql
+++ csv_queries.sql
@@ -11,7 +11,7 @@ CREATE TABLE Title (
 );
 
 CREATE TABLE Employees (
-    emp_no INT PRIMARY KEY,
+    emp_no VARCHAR(15) PRIMARY KEY,
     emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL,
     first_name VARCHAR(25) NOT NULL,
@@ -22,22 +22,22 @@ CREATE TABLE Employees (
 );
 
 CREATE TABLE Salary (
-    emp_no INT PRIMARY KEY,
+    emp_no VARCHAR(15) PRIMARY KEY,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
 );
 
 CREATE TABLE EmployeeDepartment (
-    emp_no INT NOT NULL,
+    emp_no VARCHAR(15) NOT NULL,
     dept_no VARCHAR(5),
 	PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES EmployeeDepartment(dept_no)
+	FOREIGN KEY (dept_no) REFERENCES Departments (dept_no)
 );
 
 CREATE TABLE Managers (
     dept_no VARCHAR(5) NOT NULL,
-    emp_no INT NOT NULL,
+    emp_no VARCHAR(15) NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (emp_no, dept_no) REFERENCES EmployeeDepartment(emp_no, dept_no)
 );
